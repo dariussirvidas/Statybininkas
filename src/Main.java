d40cdf14113a51f8f1eab421f77444512269e645
dariussirvidas <dsirvidas@yahoo.com>
2019-11-21 10:36:15 +0200

    pirma paveldėjimo užduotis

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,21 @@
-import com.paveldejimas.bazine.Zmogus;
 import com.paveldejimas.bazine.ZmogusImp;
-import com.paveldejimas.isvestine.Statybininkas;
 import com.paveldejimas.isvestine.StatybininkasImp;
 
 public class Main {
 
     public static void main(String[] args) {
-        Zmogus zmog1 = new ZmogusImp("vyr", 44012250237L,"Petras","Petraitis", 25, 180,"juoda");
-        Zmogus zmog2 = new ZmogusImp("mot", 47856230459L, "Laima", "Vaikulė", 45, 160,"juoda");
-        Zmogus zmog3 = new ZmogusImp("mot", 47612363615L, "Ona", "Onaitienė", 33, 175,"geltona");
-        Zmogus zmog4 = new ZmogusImp("mot",47989452125L,"Vilma","Kazlauskė", 55, 173,"raudona");
-        Zmogus zmog5 = new ZmogusImp("vyt",54621236262L, "Kazys", "Kazlauskas",42,187,"pilka");
-        Zmogus zmog6 = new ZmogusImp("vyr", 47845124578L, "Laimonas","Karalius",35,177,"ruda");
+        ZmogusImp[] zmones = new ZmogusImp[8];
+        zmones[0] = new ZmogusImp("vyr", 34012250237L,"Petras","Petraitis", 25, 180,"juoda");
+        zmones[1] = new ZmogusImp("mot", 47856230459L, "Laima", "Vaikulė", 45, 160,"juoda");
+        zmones[2] = new ZmogusImp("mot", 47612363615L, "Ona", "Onaitienė", 33, 175,"geltona");
+        zmones[3] = new ZmogusImp("mot",47989452125L,"Vilma","Kazlauskė", 55, 173,"raudona");
+        zmones[4] = new StatybininkasImp(5, "profesinis","apdailininkas", 600.5, "vyr",34621236262L, "Kazys", "Kazlauskas",42,187,"pilka");
+        zmones[5] = new StatybininkasImp(23, "aukštasis", "statybų inžinerija", 1000, "vyr", 37845124578L, "Laimonas","Karalius",35,177,"ruda");
+        zmones[6] = new StatybininkasImp(2, "vidurinis", "-", 500, "vyr", 39905120001L, "Jonas", "Jonaitis", 20, 182, "ruda");
+        zmones[7] = new StatybininkasImp(10, "Profesinis", "Statyba", 753.99, "vyr", 35511010001L, "Petras", "Petraitis", 64, 172, "ruda");
 
-        System.out.print("Būdama" + " " + zmog2.getAmzius() + " " + zmog2.getVardas() + " " + "nusprendė persidažyti plaukų spalvą, nes visada buvo " + zmog2.getPlaukuSpalva() + ". " );
-        zmog2.setPlaukuSpalva("geltona");
-        System.out.println("Plaukų spalvą ji pakeitė į " + zmog2.getPlaukuSpalva() + ".");
-
-        System.out.print("Prieš 10 metų" + " " + zmog5.getVardas() + " " + zmog5.getPavarde() + " buvo " + zmog5.getAmzius() + " darber jam ");
-        zmog5.setAmzius(52);
-        System.out.println(zmog5.getAmzius() + ".");
-        System.out.println(zmog1.getAsmensKodas());
-
-        StatybininkasImp st1 = new StatybininkasImp(10, "Profesinis", "Statyba", 753.99, "vyr", 35511010001L, "Petras", "Petraitis", 64, 172, "ruda");
-        System.out.println("Statybininko " + st1.getVardas() + " " + st1.getPavarde() + " amžius: " + st1.getAmzius() + "m. Stažas: " + st1.getStazas());
+        for (ZmogusImp zmogus : zmones)
+            if (zmogus instanceof StatybininkasImp)
+                System.out.println(zmogus.getVardas() + " " + zmogus.getPavarde() + " uždirba " + ((StatybininkasImp) zmogus).getAtlyginimas() + " Eur per mėnesį");
     }
 }
